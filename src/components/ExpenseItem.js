7132d1ec56664b1fe9d83ec40dce6f567f39429e
berganza14 <inniber@gmail.com>
2022-11-15 10:15:29 +0100

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Date</div>    
+            <div>
+                <h2>Car insurance</h2>
+                <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
